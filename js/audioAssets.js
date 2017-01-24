13e65d059576ce830f770bf36d98243584c1ef1c
rhubbard <rhubbard@reverbnation.com>
2017-01-24 13:59:06 -0500

    First commit

---
+++ js/audioAssets.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Audio Source - AWS Podcast : https://aws.amazon.com/podcasts/aws-podcast/
+var audioData = [
+    {
+        'title' : 'Devil Like Me',
+        'url' : 'https://gp1.wpc.edgecastcdn.net/802892/http_private_production_west/songs/mp3_files/12396112/original/Tripoholics_-_Mic_Spalding___Drew_Morris_-_502.314.6883._-_mspaldingjr_gmail.mp3?e8817609673e6ceae29a01a3bbc7efb2ebbcbb04d0a7bcbc1ddcb18cee7320b06ab7732824bd62f76104e6d086fa28059dbbf745db716153b1be3589fe0fde8e5c3e85f556cdbf68827583d8a24dd4f63cf231053a67dc3eea0c8ad8737162d5018ff325c76d0de5b55d2f59f383b4d6ee851b4e96c0c2e7f8940fd6be74725c20430507aa3dccb6252657960c6f0c73af8bd8a2fef14e5158865e0aa14aa6796a67848b06a886c88292e7d7d056e775f2dc1b7a2afa47cdc0b4b3644050acbc54036b26038f8a'
+    },
+    {
+        'title' : 'Black Sand',
+        'url' : 'https://gp1.wpc.edgecastcdn.net/802892/http_private_production_west/songs/mp3_files/12514742/original/8.jesus_said.mp3?e8817609673e6ceae29a01a9b7c7efb21b3ca65352303b454f3c023fbde4e49d5088f96ac365eb6e07bb390c4b9c6f2c2bc7abcd02f007acf70c5f1835310425dad27fcca628b181c3cf0c21741f0ac4b36c05358f86daed0f25e4eba7e4e18ab7042761e51bd65b5f28bfacd8e304bfb039def6db8122fac2cea606f86f76bb29420c831231af'
+    },
+];
+
+module.exports = audioData;
